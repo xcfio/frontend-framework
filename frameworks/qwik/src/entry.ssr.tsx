62bd9b379c79c8205288f954649e4d0061f1d0f9
xcfio <119097812+xcfio@users.noreply.github.com>
2026-06-06 11:12:53 +0600

    Add Next.js

--- frameworks/qwik/src/entry.ssr.tsx
+++ frameworks/qwik/src/entry.ssr.tsx
@@ -10,22 +10,19 @@
  * - npm run build
  *
  */
-import {
-  renderToStream,
-  type RenderToStreamOptions,
-} from "@builder.io/qwik/server";
-import Root from "./root";
+import { renderToStream, type RenderToStreamOptions } from "@builder.io/qwik/server"
+import Root from "./root"
 
 export default function (opts: RenderToStreamOptions) {
-  return renderToStream(<Root />, {
-    ...opts,
-    // Use container attributes to set attributes on the html tag.
-    containerAttributes: {
-      lang: "en-us",
-      ...opts.containerAttributes,
-    },
-    serverData: {
-      ...opts.serverData,
-    },
-  });
+    return renderToStream(<Root />, {
+        ...opts,
+        // Use container attributes to set attributes on the html tag.
+        containerAttributes: {
+            lang: "en-us",
+            ...opts.containerAttributes
+        },
+        serverData: {
+            ...opts.serverData
+        }
+    })
 }
